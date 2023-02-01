59a45d9d3b99c450c74c5d4aebbdff4c14ee2e3c
Peter Kaagman <prjv.kaagman@gmail.com>
2023-02-01 15:55:01 +0100

    Objects with Moose

--- msgraph_lwp.pl
+++ msgraph_lwp.pl
@@ -1,70 +1,81 @@
-#! /usr/bin/perl -W
+#! /usr/bin/perl
 
 use strict;
+use warnings;
+use v5.10;
+
 use Data::Dumper;
-use JSON;
 use Config::Simple;
 use FindBin;
-#use lib "$FindBin::Bin/../lib";
-use LWP::UserAgent;
+use POSIX qw(strftime);
+use Getopt::Long;
+use lib "$FindBin::Bin/lib";
+
+use MsGroups;
+use Logger;
+
 
 my %config;
 Config::Simple->import_from("$FindBin::Bin/groups.cfg",\%config) or die("No config: $!");
 
-my $ua = LWP::UserAgent->new(
-	'send_te' => '0',
+my $now = time();
+my $ts = strftime('%Y-%m-%dT%H:%M:%S', localtime($now));
+
+
+my $filter;
+my $verbose;
+my $no_owner;
+
+GetOptions(
+	"filter=s"	=>	\$filter,
+	"verbose"	=>	\$verbose,
+	"no_owner"	=>	\$no_owner,
+) or die("Error in command line options: $!");
+
+# Start of the logger
+my $logger = Logger->new(
+	'filename' => "$FindBin::Bin/Log/$FindBin::Script-$ts.log",
+	'verbose' => $verbose
+); 
+$logger->make_log("$FindBin::Bin/$FindBin::Script started.");
+
+if ($filter){
+	$logger->make_log("Filter is: $filter");
+}
+if ($verbose){
+	$logger->make_log("Verbose is set.");
+}
+if ($no_owner){
+	$logger->make_log("NoOwner is set.");
+}
+
+my $session = MsGroups->new(
+	'app_id'         => $config{'APP_ID'},
+	'app_secret'     => $config{'APP_SECRET'},
+	'tenant_id'      => $config{'TENANT_ID'},
+	'login_endpoint' => $config{'LOGIN_ENDPOINT'},
+	'graph_endpoint' => $config{'GRAPH_ENDPOINT'},
+	'blaat' => $config{'GRAPH_ENDPOINT'},
+	'filter'         => $filter,
 );
 
-sub login_app { #	{{{1
-	my $url = "$config{'LOGIN_ENDPOINT'}/$config{'TENANT_ID'}/oauth2/token";
-	my $r = HTTP::Request->new(
-		'POST' => $url,
-			[
-				'Accept'		=>	'*/*',
-				'User-Agent'	=>	'curl/7.55.1',
-				'Content-Type'	=>	'application/x-www-form-urlencoded'
-			],
-			"grant_type=client_credentials&client_id=$config{'APP_ID'}&client_secret=$config{'APP_PASS'}&scope=$config{'GRAPH_ENDPOINT'}/.default&resource=$config{'GRAPH_ENDPOINT'}"
-	);
-
-	my $result = $ua->request($r);
-
-	if ($result->is_success){
-		return decode_json($result->decoded_content)
-	}else{
-		print Dumper $result;
-		die $result->status_line;
+if ($session->_get_access_token){
+	my $groups = $session->fetch_groups();
+	my $count = scalar @$groups;
+	$logger->make_log("$count groups fetched.");
+	while (my ($i, $group) = each @{$groups}){
+		$logger->make_log("$$group{'id'} => $$group{'displayName'}");
 	}
-	
-}#	}}}
 
-sub fetch { #	{{{1
-	my $token = shift;
-	my $url = shift;
-	my $r  = HTTP::Request->new(
-    	'GET' => $url,
-			[
-        		'Accept'        => '*/*',
-        		'Authorization' => "Bearer $token",
-        		'User-Agent'    => 'curl/7.55.1',
-        		'Content-Type'  => 'application/json'
-			],
-	);	
-	my $result = $ua->request($r);
-	if ($result->is_success){
-		return decode_json($result->decoded_content)
-	}else{
-		print Dumper $result;
-		die $result->status_line;
+	if ($no_owner){
+		$groups = $session->fetch_groups_no_owner($groups);
+		$count = scalar @$groups;
+		$logger->make_log("$count Groups without owner.");
+		while (my ($i, $group) = each @{$groups}){
+			$logger->make_log("$$group{'id'} => $$group{'displayName'}");
+		}
 	}
-	
-}#	}}}
-
-my $token_request = login_app();
-print Dumper $token_request;
-
-if ($$token_request{'access_token'}){
-	my $url = "$config{'GRAPH_ENDPOINT'}/v1.0/groups";
-	my $groups = fetch($$token_request{'access_token'}, $url);
-	print Dumper $groups;
+}else{
+	print "No token!\n";
 }
+# vim: set foldmethod=marker
