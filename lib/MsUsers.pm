0e286b7b4d892ff06e2397475199091e7cfd4348
peter kaagman <prjv.kaagman@gmail.com>
2024-09-19 12:23:39 +0200

    Ook niet active users worden opgehaald in MsUsers Fixes #10 Bij het maken van de URL van user_fetch wordt nu default een filter toegevoegd op active users

--- lib/MsUsers.pm
+++ lib/MsUsers.pm
@@ -16,10 +16,24 @@ extends 'MsGraph';
 
 sub users_fetch {
     my $self = shift;
+    my $showAll = shift;
     my @users;
     my @parameters;
-    push(@parameters,$self->_get_filter) if ($self->_get_filter);
     push(@parameters,$self->_get_select) if ($self->_get_select);
+    # Voeg een filter toe op accountEnabled
+    if ($self->_get_filter){
+        if ($showAll){
+            # _get_Filter en showAll -> alleen _get_filter dus
+            push(@parameters,$self->_get_filter) if ($self->_get_filter);
+        }else{
+            # filteren op active en _get_filter -> filter combineren
+            my $filter = $self->_get_filter . 'and accountEnabled eq true';
+            push(@parameters, $filter);
+        }
+    }else{
+        push(@parameters,'$filter=accountEnabled eq true') if (! $showAll);
+    }
+
     
     my $url = $self->_get_graph_endpoint . "/v1.0/users/?". join( '&', @parameters);
 
